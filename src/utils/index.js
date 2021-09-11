e7ed38cdb7cef71c7c2a463836ae2d8a72807c74
Guy <shefer.guy1@gmail.com>
2021-09-12 01:35:18 +0300

    add utils

---
+++ src/utils/index.js
@@ -0,0 +1,23 @@
+const baseDir =  document.location.hostname === "localhost" ?
+  '' :'/portfolio/images/tech/';
+
+const techImages = {
+    mongodb: baseDir + "mongodb.png",
+    typescript: baseDir + "ts.png",
+    mongoose: baseDir + "mongoose.jpg",
+    css3: baseDir + "css3.png",
+    expressjs: baseDir + "expressjs.png",
+    html5: baseDir + "html5.png",
+    javascript: baseDir + "javascript.png",
+    jwt: baseDir + "jwt.jpg",
+    nodejs: baseDir + "nodejs.png",
+    reactjs: baseDir + "reactjs.png",
+    redux: baseDir + "redux.png",
+    python: baseDir + "python.png",
+    django: baseDir + "django.jpg",
+    firebase: baseDir + "firebase.png",
+    reactBootstrap: baseDir + "react-bootstrap.png",
+    djangoRestFramework: baseDir + "djangoRestFramework.png",
+    jest: baseDir + "jest.png",
+}
+export default techImages;
---
+++ src/utils/projects.js
@@ -0,0 +1,100 @@
+export default [
+    {
+        "name": "Pokemon - Math The Memory",
+        "description": "An addictive Pokemon memory game made with pure Javascript, CSS, and HTML5.",
+        "git_url": "https://github.com/GuyShefer/Match-The-Memory",
+        "demo_url": "https://zen-jones-6acf62.netlify.app/",
+        "technologies": ["javascript", "html5", "css3"]
+    },
+    {
+        "name": "Snake",
+        "description": "In Snake, the player moves a snake around the board using the arrow keys. As the snake finds food, it eats the food and thereby grows larger.",
+        "git_url": "https://github.com/GuyShefer/Snake",
+        "demo_url": "https://mystifying-hoover-35ca2f.netlify.app/",
+        "technologies": ["reactjs", "javascript", "html5", "css3"]
+    },
+    {
+        "name": "Milk & Honey Assignment",
+        "description": "An e-commerce site that retrieves products from 'Fake Store API.",
+        "git_url": "https://github.com/GuyShefer/Milk-And-Honey-Distillery",
+        "demo_url": "https://milk-and-honey-distillery.herokuapp.com/",
+        "technologies": ["reactjs", "javascript", "html5", "css3", "redux", "firebase", "reactBootstrap"]
+    },
+    {
+        "name": "URL Shortner",
+        "description": "Django API service created to shorten URLs.",
+        "git_url": "https://github.com/GuyShefer/URL-Shortener",
+        "demo_url": "",
+        "technologies": ["python", "django", "html5"]
+    },
+    {
+        "name": "Candidate Finder",
+        "description": "Django Back-end API service is designed to find the best candidates for a job position.",
+        "git_url": "https://github.com/GuyShefer/CandidateFinder",
+        "demo_url": "",
+        "technologies": ["python", "django", "html5", "djangoRestFramework"]
+    },
+    {
+        "name": "Havaya Jewlery",
+        "description": "Full-stack project for a jewelry store that involves authentication and authorization.",
+        "git_url": "https://github.com/GuyShefer/AppleSeeds-Final-Project",
+        "demo_url": "https://final-project-appleseeds.herokuapp.com/",
+        "technologies": ["javascript", "reactjs", "redux", "html5", "css3", "nodejs", "jwt", "expressjs", "mongodb", "mongoose"]
+    },
+    {
+        "name": "Covid-19",
+        "description": "Fetching Corona data from API and displaying graphs of Corona status in each country.",
+        "git_url": "https://github.com/GuyShefer/Covid-19-Project",
+        "demo_url": "https://covid-19-worldproject.netlify.app/",
+        "technologies": ["javascript", "html5", "css3"]
+    },
+    {
+        "name": "Bank API",
+        "description": "A server-side project designed to serve the functionality of a bank users.",
+        "git_url": "https://github.com/GuyShefer/BankAPI/blob/main/src/controllers/users.controller.js",
+        "demo_url": "",
+        "technologies": ["javascript", "nodejs", "expressjs"]
+    },
+    {
+        "name": "Bank API",
+        "description": "A server-side project designed to serve the functionality of a bank users.",
+        "git_url": "https://github.com/GuyShefer/BankAPI/blob/main/src/controllers/users.controller.js",
+        "demo_url": "",
+        "technologies": ["javascript", "nodejs", "expressjs", "jest"]
+    },
+    {
+        "name": "Next Trip",
+        "description": "An appliction used to filter data from APIs and manipulate the data with the user requests.",
+        "git_url": "https://github.com/GuyShefer/Next-Trip",
+        "demo_url": "https://loving-heisenberg-3ab39c.netlify.app/",
+        "technologies": ["reactjs", "javascript", "html5", "css3"]
+    },
+    {
+        "name": "Dice Game",
+        "description": "The game is played between two people, with each player rolling two dice, and the first player to reach 100 points wins.",
+        "git_url": "https://github.com/GuyShefer/Dice-Game-Project",
+        "demo_url": "https://quizzical-payne-bfb9b9.netlify.app/",
+        "technologies": ["reactjs", "javascript", "html5", "css3"]
+    },
+    {
+        "name": "Minecraft 2D Game",
+        "description": "My project is a pre-generated, 2D game board with the ability to add and remove tiles.",
+        "git_url": "https://github.com/GuyShefer/Minecraft-2D-Project",
+        "demo_url": "https://minecraft-2d.netlify.app/main.html",
+        "technologies": ["javascript", "html5", "css3"]
+    },
+    {
+        "name": "Sammy Ofer Stadium",
+        "description": "Website devoted to the 'Sami Ofer' stadium in Haifa.",
+        "git_url": "https://github.com/GuyShefer/Sammy-Ofer-Stadium-Website",
+        "demo_url": "https://heuristic-elion-0e47e4.netlify.app/",
+        "technologies": ["html5", "css3"]
+    },
+    {
+        "name": "Amazon Clone Site",
+        "description": "A website devoted to cloning an Amazon page.",
+        "git_url": "https://github.com/GuyShefer/Amazon-Project",
+        "demo_url": "https://loving-brahmagupta-1f014d.netlify.app/",
+        "technologies": ["html5", "css3"]
+    },
+]
